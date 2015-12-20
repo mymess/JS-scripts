4fad87b2814e77b48fbde06ba1438552f2fcf738
Joan Climent <joandelason@gmail.com>
2015-12-20 22:25:59 +0100

    solar system coordinates

--- jSky.js
+++ jSky.js
@@ -5,11 +5,46 @@ var SKY = SKY || {}
 //http://www.astro-phys.com/api/de406/states?date=1000-1-20&bodies=mars
 
 
-var url = 'http://www.astro-phys.com/api/de406/states?date=2003-09-25T10:49&bodies=sun,mercury,venus,moon,mars,jupiter,saturn,uranus,neptune,pluto';
-var url2 = 'http://www.astro-phys.com/api/de406/states?date=2452907.95094&bodies=sun,moon' 
+//var url = 'http://www.astro-phys.com/api/de406/states?date=2003-09-25T10:49&bodies=sun,mercury,venus,moon,mars,jupiter,saturn,uranus,neptune,pluto';
+//var url2 = 'http://www.astro-phys.com/api/de406/states?date=2452907.95094&bodies=sun,mercury,venus,moon,mars,jupiter,saturn,uranus,neptune,pluto'; 
 
-//{"date": 2452907.9506899999, "results": {"mercury": [[19463872.423888482, 37421874.914288841, 18057065.658873655], [-4675250.709463357, 1510708.0421961588, 1291774.8804996102]], "sun": [[445680.99614797306, -469235.66244900582, -210842.90240303476], [744.82291327944824, 819.61886509338103, 328.19636325781693]], "neptune": [[3013896576.9082661, -3065887420.0188046, -1329919054.9011033], [345661.05763426982, 296821.37208586052, 112884.98859413425]], "pluto": [[-847267801.01731074, -4375466103.3217125, -1110169105.1944985], [470591.16667198745, -108394.18506923175, -175613.79806991192]], "moon": [[150038161.54281148, 4167913.9208064275, 1829889.7135109843], [-135240.5546763757, 2270459.9097901117, 979393.06841622444]], "jupiter": [[-707055433.67959797, 346003510.40025777, 165522646.76293522], [-552014.71571031539, -868059.06901469, -358641.91154183605]], "uranus": [[2624606445.661747, -1312116639.2143543, -611796043.99953485], [279795.70566095895, 448306.09916104906, 192389.589152467]], "mars": [[205956239.32522061, -22946826.715117842, -16074034.79773663], [353298.75677190966, 2054186.3730860082, 932621.57810260833]], "venus": [[-96205517.889507368, -46440660.771915838, -14776685.651497068], [1325112.4614611601, -2453403.5819634269, -1187635.3085047482]], "saturn": [[-142468154.55159792, 1239077249.1837988, 517920159.6476981], [-874424.5127583039, -96974.70034699222, -2410.359462898391]]}, "unit": "km"}
 
+var url = 'http://api.nginov.com/shared/ws/astro/?lat=39.43&lon=.45&y=2015&m=02&d=17&h=11&i=15&s=00&out=json'
+
+
+var data = {"date": 2452907.9506899999, "results": {"mercury": [[19463872.423888482, 37421874.914288841, 18057065.658873655], [-4675250.709463357, 1510708.0421961588, 1291774.8804996102]], "sun": [[445680.99614797306, -469235.66244900582, -210842.90240303476], [744.82291327944824, 819.61886509338103, 328.19636325781693]], "neptune": [[3013896576.9082661, -3065887420.0188046, -1329919054.9011033], [345661.05763426982, 296821.37208586052, 112884.98859413425]], "pluto": [[-847267801.01731074, -4375466103.3217125, -1110169105.1944985], [470591.16667198745, -108394.18506923175, -175613.79806991192]], "moon": [[150038161.54281148, 4167913.9208064275, 1829889.7135109843], [-135240.5546763757, 2270459.9097901117, 979393.06841622444]], "jupiter": [[-707055433.67959797, 346003510.40025777, 165522646.76293522], [-552014.71571031539, -868059.06901469, -358641.91154183605]], "uranus": [[2624606445.661747, -1312116639.2143543, -611796043.99953485], [279795.70566095895, 448306.09916104906, 192389.589152467]], "mars": [[205956239.32522061, -22946826.715117842, -16074034.79773663], [353298.75677190966, 2054186.3730860082, 932621.57810260833]], "venus": [[-96205517.889507368, -46440660.771915838, -14776685.651497068], [1325112.4614611601, -2453403.5819634269, -1187635.3085047482]], "saturn": [[-142468154.55159792, 1239077249.1837988, 517920159.6476981], [-874424.5127583039, -96974.70034699222, -2410.359462898391]]}, "unit": "km"}
+
+for( var body in data.results){
+  	  console.log( body )
+	  var p   = data.results[body][0] ;
+	  var v   = data.results[body][1] ;
+
+	  var x = p[0];
+	  var y = p[1];
+	  var z = p[2];
+
+
+
+	  console.log('Position:\nx='+p[0]+'\ny='+p[1]+'\nz='+p[2]);
+	  console.log('Velocity:\nx='+v[0]+'\ny='+v[1]+'\nz='+v[2]);
+
+	  // for (var item in p ){
+
+	  // 	var nums = Object.keys(item);
+
+	  // 	for( var num in nums ) {
+	  // 	console.log( item );
+	  // 	console.log( p[item] )
+
+	  // 	}
+	  // }
+	  //var pos = Object.keys(p)[0];
+	  
+
+	  // console.log( "p--> " + p );
+	  // console.log( "pos--> " + pos );
+}
+/*
 $.getJSON(url, function(data) {
   console.log(data);
   for( var body in data.results){
@@ -31,10 +66,13 @@ $.getJSON(url, function(data) {
 	  //console.log('Velocity:\nx='+v[0]+'\ny='+v[1]+'\nz='+v[2]);
   }
 });
-
+*/
 
 
 SKY = {
+
+	bodies: [],
+	stars: [],
 
 	init: function(utc, lon, lat){
 		
@@ -48,24 +86,76 @@ SKY = {
 		this.HOURS2DEG = 15;
 		this.latRad = this.lat*this.DEG2RAD;
 
-		this.objects = [];  //sun, moon, planets
-		this.stars   = [];
+		this.loadBodiesData();
+		this.bodies = [];
+		//this.bodies = [];  //sun, moon, planets
+		//this.stars   = [];
 	},
 
 	update: function(){
-		for(var i=0; i<this.objects.length; i++ ){
-			this.calculateLocalCoordinates( this.objects[i] )			
+		for(var i=0; i<this.bodies.length; i++ ){
+			this.calculateLocalCoordinates( this.bodies[i] )			
 		}
 		for(var i=0; i<this.stars.length; i++){
 			this.calculateLocalCoordinates( this.stars[i] )
 		}
 	},
 
-	//day is a fraction
-	getJulianDay: function(year, month, day){
-		//return jd
+	
+	getJulianDay: function(){
+		var now    = this.utc;
+	    var year   = now.getUTCFullYear();
+	    var month  = now.getUTCMonth() + 1;
+	    var day    = now.getUTCDate();
+	    var hour   = now.getUTCHours();
+	    var minute = now.getUTCMinutes();
+	    var second = now.getUTCSeconds();
+
+
+	    var dayFract = day + hour/24 + minute/(24*60) + second/(24*60*60)
+
+	    return this.date2Julian(month, dayFract, year);
 	},
-	calculateLocalCoordinates: function ( body ) {		
+
+	date2Julian: function(inMonth, inDay, inYear)
+	{
+	        var             A,B;
+	        var             theMonth = inMonth;
+	        var             theYear = inYear;
+	  
+	        if ( inMonth <= 2 )
+	                { --theYear;  theMonth += 12;  }
+	   
+	        A = Math.floor(theYear/100.0);
+	  
+	        if ( inYear < 1582 )
+	                B = 0;
+	        else if (inYear > 1582 )
+	                B = 2 - A + Math.floor(A/4.0);
+	        else
+	    {
+	                if ( inMonth < 10 )
+	                        B = 0;
+	                else if ( inMonth > 10 )
+	                        B = 2 - A + Math.floor(A/4.0);
+	                else
+	                {
+	                        if ( inDay < 5 )
+	                                B = 0;
+	                        else if ( inDay >= 15 )
+	                                B = 2 - A + Math.floor(A/4.0);
+	                        else
+	                                { return -1; } /* error, days falls on 10/5/1582 - 10/14/1582 */
+	                } /* end middle else */   
+	        } /* end outer else */
+
+	        /* Julian Day */
+	        return Math.floor(365.25 * (theYear + 4716.0)) + Math.floor(30.6001 * (theMonth + 1.0)) + inDay + B - 1524.5;
+	},
+
+
+
+	calculateLocalCoordinates: function ( item ) {		
 		var ans = coord_to_horizon (item.ra, item.dec)
 		item.prototype.az  = ans[0];
 		item.prototype.alt = ans[1];
@@ -74,7 +164,7 @@ SKY = {
 	coord_to_horizon: function ( ra, dec )
 	{
 	    // compute hour angle in degrees
-	    var ha = mean_sidereal_time( ) - ra*this.HOURS2DEG;
+	    var ha = mean_sidereal_time( ) - ra * this.HOURS2DEG;
 	    if (ha < 0) ha = ha + 360;
 
 	    // convert degrees to radians
@@ -143,11 +233,12 @@ SKY = {
 	        
 	    return mst;
 	},
-	Object:  function(name, ra, dec, magnitude){
+
+
+	Body:  function(name, ra, dec){
 		this.name      = name;
 		this.ra 	   = ra;
-		this.dec 	   = dec;	
-		this.magnitude = magnitude;		
+		this.dec 	   = dec;				
 	},
 
 	Star: function(name, ra, dec, magnitude, color){
@@ -156,13 +247,68 @@ SKY = {
 		this.dec 	   = dec;	
 		this.magnitude = magnitude;		
 		this.color 	   = color;	
+	},
+
+	loadBodiesData: function(){
+		var url1 = 'http://www.astro-phys.com/api/de406/states?date=';
+		var url2 = '&bodies=sun,mercury,venus,earth,moon,mars,jupiter,saturn,uranus,neptune,pluto&unit=km'; 
+
+		var url = url1 + this.getJulianDay() + url2;
+
+		var array = [];
+
+		$.getJSON(url, function(data) {
+
+			for( var body in data.results){
+				var pos   = data.results[body][0] ;
+				//var v   = data.results[body][1] ;
+
+				var x = pos[0];
+				var y = pos[1];
+				var z = pos[2];
+
+				//var ans = this.calculatePolarCoordinates(x, y, z);
+				ra  = Math.atan2(y, x)*SKY.RAD2DEG + 180;
+				dec = Math.atan2(z, Math.sqrt( x*x + y*y ))*SKY.RAD2DEG ;
+
+				var newBody = new SKY.Body( body, ra, dec );
+				newBody.x = x;
+				newBody.y = y;
+				newBody.z = z;
+				newBody.distance = Math.sqrt( x*x + y*y + z*z );
+				console.log( newBody );
+				console.log( SKY.bodies )
+				//SKY.bodies.add[ newBody ];
+			}
+
+		});
+	},
+
+	//input: rectangular coordinates X, Y, Z
+	//output: array containing RA and declination in degrees, and distance in km
+	calculatePolarCoordinates: function(x, y, z){
+		var ra = Math.atan2(y, x);
+		var dec = Math.atan2(z, Math.sqrt( x*x + y*y ));
+		var distance = Math.sqrt( x*x + y*y + z*z );
+
+		var ans = [ra*this.RAD2DEG, dec*this.RAD2DEG, distance];
+
+		return ans;
+	},
+
+	log: function(){
+		for( var body in this.bodies){
+			console.log( "Name -> " + body.name );
+			console.log( "Dec -> " + body.dec );
+			
+		}
 	}
 }
 
 
 var year = 2015;
-var month = 11;
-var day = 12.1213;
+var month = 12;
+var day = 20.1213;
 
 
 var hours 		 = (day - Math.floor(day)) * 24;
@@ -174,19 +320,15 @@ var utc = new Date(year, month,
 					Math.floor(day), Math.floor(hours), 
 					Math.floor(minutes), Math.floor(seconds), 
 					milliseconds );
-/*
-var SKY = Object.create(SKY);
 
-SKY.objects.add( new SKY.Object("Sun", 12, 12, -27) );
+//var SKY = Object.create(SKY);
+
+//SKY.objects.add( new SKY.Object("Sun", 12, 12, -27) );
 //...
 
 //SKY.stars.add( new SKY.Star("..."))
 
-var model = SKY.init(utc, 120.12, 12.12 );
+var model = SKY.init(utc, 0.12, 39.45 );
 model.update();
+model.log();
 
-
-SKY.loader = function(){
-
-}
-*/
